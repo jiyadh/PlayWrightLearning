c525e4eb03c23f1e25480ec7dff5d65bdbc500ea
jiyadh <muhammedjiyadh@outlook.com>
2025-03-27 10:47:44 +0100

    changed test report

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html"], ["list"]],
+  reporter: [
+    ["list"], // Default console reporter
+    ["junit", { outputFile: "test-results/results.xml" }], // JUnit XML reporter
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
